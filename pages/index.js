9b02aae0cdd871fb451749d6b33e669989f31a05
Rahul Shah <rahulshah7@outlook.com>
2020-01-14 01:53:50 +0000

    Add walk through of code (yarn dev, go to browser to view)

--- pages/index.js
+++ pages/index.js
@@ -36,5 +36,28 @@ export default () => {
       }, 2000)
     );
 
-  return <h1>hello</h1>;
+  return (
+    <>
+      <h1>Auth example</h1>
+      <ul>
+        <li>open dev tools console</li>
+        <li>
+          the signup api call is commented out above, the db already has this
+          user
+        </li>
+        <li>should see login details in console</li>
+        <li>should see entries data in console after 2000ms delay</li>
+        <li>response should be 200 OK</li>
+        <br />
+        <li>
+          try changing the sign in details in the code to fake ones, response
+          should be 401 - remember to undo changes after.
+        </li>
+        <li>
+          you may see multiple calls being made in repetition - This is clearly
+          not the place to be making the api call in react/next.js
+        </li>
+      </ul>
+    </>
+  );
 };
